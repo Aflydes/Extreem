f45a5d301832f036e3932d3958af25654c874b7e
Aflydes <aflydes@gmail.com>
2022-03-19 10:25:59 +0300

    start

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: false,
+        pagination: {
+            el: '.swiper-pagination',
+          },
+      
+      });
+}
