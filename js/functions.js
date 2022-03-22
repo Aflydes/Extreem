2de8e9dc4ba9b525b52476af092adc2df691884c
Aflydes <aflydes@gmail.com>
2022-03-22 18:23:29 +0300

    add functions

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,26 @@
-window.addEventListener('scroll', function() {
+let links = document.querySelectorAll('.main-page__nav-link');
+    slides = document.querySelectorAll('.wow');
+    park = document.querySelector('#park');
+    beach = document.querySelector('#beach');
+    glamping = document.querySelector('#glamping');
+    calendar = document.querySelector('#calendar');
+
+window.addEventListener('scroll', function () {
     const wow = new WOW({
-        boxClass: 'wow', 
-        animateClass: 'animate__animated', 
-        offset: 0, 
-        mobile: true, 
+        boxClass: 'wow',
+        animateClass: 'animate__animated',
+        offset: 0,
+        mobile: true,
         live: true
     })
     wow.init();
 });
 
-  let links = document.querySelectorAll('.main-page__nav-link');
-  let slides = document.querySelectorAll('.wow');
-
-  links.forEach( function(link){
-    link.addEventListener('click', function(){
-        slides.forEach( function(slide){
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        slides.forEach(function (slide) {
             slide.classList.remove('animate__animated');
         });
-    });
-  });
 
+    });
+});
