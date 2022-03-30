ccd0698c1f5dbf978e908efc822653ebf313a872
Aflydes <aflydes@gmail.com>
2022-03-30 12:24:25 +0300

    fix

--- js/functions.js
+++ js/functions.js
@@ -95,28 +95,30 @@ $('.main-page__nav-link').click( function(){
     markerPos = markerPos + $('[data-slide='+ currentSlide +']').width()/2 - 1.5;
     $('.screen-nav-marker').css('right', markerPos+ 'px');
 });
-$('.main-page').on("swipe", function(){
-    if(direction  == "swipeUp"){
-        if (currentSlide != "last"){
-            $('#'+ currentSlide).find('.animation-inner').addClass("animation-down");
-            $('#'+ currentSlide).next().removeClass('hidden');
-            $('#'+ currentSlide).addClass('hidden');              
-            currentSlide = $('#'+ currentSlide).next().attr('id');
-            $('#'+ currentSlide).find('.animation-inner').removeClass("animation-down");
-            markerPos = $('[data-slide='+ currentSlide +']').position().top;
-            markerPos = markerPos + $('[data-slide='+ currentSlide +']').width()/2 - 1.5;
-            $('.screen-nav-marker').css('right', markerPos+ 'px');
-        }  
-    } else if(direction  == "swipeDown") {
-        if (currentSlide != "welcome") {
-            $('#'+ currentSlide).find('.animation-inner').addClass("animation-down");
-            $('#'+ currentSlide).addClass('hidden');
-            currentSlide = $('#'+ currentSlide).prev().attr('id');
-            $('#'+ currentSlide).removeClass('hidden');
-            $('#'+ currentSlide).find('.animation-inner').removeClass("animation-down");
-            markerPos = $('[data-slide='+ currentSlide +']').position().top;
-            markerPos = markerPos + $('[data-slide='+ currentSlide +']').width()/2 - 8;
-            $('.screen-nav-marker').css('right', markerPos+ 'px');
-        }  
+$('.main-page').swipe({
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+        if(direction  == "swipeUp"){
+            if (currentSlide != "last"){
+                $('#'+ currentSlide).find('.animation-inner').addClass("animation-down");
+                $('#'+ currentSlide).next().removeClass('hidden');
+                $('#'+ currentSlide).addClass('hidden');              
+                currentSlide = $('#'+ currentSlide).next().attr('id');
+                $('#'+ currentSlide).find('.animation-inner').removeClass("animation-down");
+                markerPos = $('[data-slide='+ currentSlide +']').position().top;
+                markerPos = markerPos + $('[data-slide='+ currentSlide +']').width()/2 - 1.5;
+                $('.screen-nav-marker').css('right', markerPos+ 'px');
+            }  
+        } else if(direction  == "swipeDown") {
+            if (currentSlide != "welcome") {
+                $('#'+ currentSlide).find('.animation-inner').addClass("animation-down");
+                $('#'+ currentSlide).addClass('hidden');
+                currentSlide = $('#'+ currentSlide).prev().attr('id');
+                $('#'+ currentSlide).removeClass('hidden');
+                $('#'+ currentSlide).find('.animation-inner').removeClass("animation-down");
+                markerPos = $('[data-slide='+ currentSlide +']').position().top;
+                markerPos = markerPos + $('[data-slide='+ currentSlide +']').width()/2 - 8;
+                $('.screen-nav-marker').css('right', markerPos+ 'px');
+            }  
+        }
     }
 });
